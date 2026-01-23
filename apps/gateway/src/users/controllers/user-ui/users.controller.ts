10e39934f808c8abf65e176e2bf34ce830be23c2
Cristobal-Rivera_lennox <Cristobal-Rivera_lennox>
2026-01-23 13:40:26 -0600

    feat(User Pagination)
    Added the user pagination using cursors.

--- apps/gateway/src/users/controllers/user-ui/users.controller.ts
+++ apps/gateway/src/users/controllers/user-ui/users.controller.ts
@@ -1,28 +1,36 @@
 import { UsersService } from '../../users.service';
-import { Controller, Get, Param, Post, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Res, UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { DashboardUsersDto } from '../../dto/dashboard-users.dto';
-import { ApiResponseBuilder } from '@workflow-automation/shared-types';
+import { DashboardUserDataDto, DashboardUsersDto } from '../../dto/dashboard-users.dto';
+import { ApiResponse, ApiResponseBuilder, CursorPaginatedResponse } from '@workflow-automation/shared-types';
 import { JwtAuthGuard } from '../../../auth/guards/jwt-auth.guard';
+import { HttpStatusCode } from 'axios';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('dashboard/:organizationId')
-  @UseGuards(JwtAuthGuard)
   async getDashboardData(
     @Param('organizationId') organizationId: string,
     @Res() res: Response,
-  ): Promise<Response<ApiResponseBuilder<DashboardUsersDto>>> {
-    const apiResponse = new ApiResponseBuilder<DashboardUsersDto>();
-    const result = await this.usersService.getDashboardData(organizationId);
-    if (result.users.length === 0) {
+    @Query('cursor') cursor: string | null = null,
+    @Query('pageSize') pageSize: number = 10,
+    @Query('action') action: 'next' | 'prev' | null = null,
+  ): Promise<Response<ApiResponse<CursorPaginatedResponse<DashboardUserDataDto>>>> {
+    const apiResponse = new ApiResponseBuilder<CursorPaginatedResponse<DashboardUserDataDto>>();
+    const result = await this.usersService.getDashboardData(
+      organizationId,
+      cursor,
+      pageSize,
+      action
+    );
+    if (result.items.length === 0) {
       apiResponse.setStatusCode(404).setMessage('No user data found for the organization');
-      return res.status(404).json(apiResponse.build());
+      return res.status(HttpStatusCode.NotFound).json(apiResponse.build());
     } else {
       apiResponse
-        .setStatusCode(200)
+        .setStatusCode(HttpStatusCode.Ok)
         .setMessage('User dashboard data retrieved successfully')
         .setData(result);
       return res.status(200).json(apiResponse.build());
