e5684292f027f86675486dd92c16a700d8e3f192
Daniel Limon <cervantesdaniellimon50m@gmail.com>
2026-04-09 11:35:29 -0600

    Jest configurado en el web-client

--- apps/web-client/jest.setup.js
+++ apps/web-client/jest.setup.js
@@ -1,5 +1,1 @@
-/**
- * Runs after the test environment is set up.
- * jest-dom matchers are loaded via setupFilesAfterEnv.
- * window.location is patched in the custom environment (jest.environment.js).
- */
+import '@testing-library/jest-dom';
